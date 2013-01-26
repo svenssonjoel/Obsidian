d8fd46104d7c812f88418aa9f31a98b20654737c
Joel Svensson <svenssonjoel@yahoo.se>
2013-01-26 18:58:52 +0100

    pushing Niklas' chanages into the master branch

--- Obsidian/CodeGen/InOut.hs
+++ Obsidian/CodeGen/InOut.hs
@@ -3,10 +3,11 @@
              MultiParamTypeClasses,
              TypeOperators,
              TypeFamilies ,
-             ScopedTypeVariables, 
-             CPP #-}
+             ScopedTypeVariables
+             #-}
 
-{- Joel Svensson 2012
+{- Joel Svensson 2012, 2013
+   Niklas Ulvinge 2013
 
   Notes:
 
@@ -50,104 +51,42 @@ type Inputs = [(Name,Type)]
 class ToProgram a b where
   toProgram :: Int -> (a -> b) -> Ips a b -> (Inputs,CG.Program ())
 
-{-
-instance ToProgram (Distrib (Pull (Exp t))) (GProgram a) where { \
-  toProgram i f (Distrib blkf)  =      \
-    ([(nom,Pointer t)],CG.runPrg (f input)) \
-     where {nom = "input" ++ show i; \
-            n   = len (blkf (variable "X")); \
-            input = namedGlobal  nom n;}}  \
-;\
-instance ToProgram (Distrib (Pull (Exp t))) (Final (GProgram a)) where { \
-  toProgram i f (Distrib blkf)  =      \
-    ([(nom,Pointer t)],CG.runPrg (cheat (f input))) \
-     where  {nom = "input" ++ show i; \
-            n   = len (blkf (variable "X")); \
-            input = namedGlobal  nom n;}} 
+typeOf_ a = typeOf (Literal a)
 
+instance (Scalar t) => ToProgram (Exp t) (GProgram b) where
+  toProgram i f a = ([(nom,t)],CG.runPrg (f input))
+    where nom = "s" ++ show i
+          input = variable nom
+          t = typeOf_ (undefined :: t)
 
--} 
+instance (Scalar t) => ToProgram (GlobPull (Exp t)) (GProgram a) where
+  toProgram i f (GlobPull ixf) = ([(nom,Pointer t)],CG.runPrg (f input)) 
+      where nom = "input" ++ show i 
+            input = namedGlobal nom
+            t = typeOf_ (undefined :: t)
 
-#define toprgBase(t) \
-instance ToProgram (Exp t) (GProgram b) where {\
-  toProgram i f a = \
-    ([(nom,t)],CG.runPrg (f input)) \
-    where {nom = "s" ++ show i; \
-           input = variable nom;}}\
-;\
-instance ToProgram (GlobPull (Exp t)) (GProgram a) where { \
-  toProgram i f (GlobPull ixf) = \
-    ([(nom,Pointer t)],CG.runPrg (f input)) \
-      where {nom = "input" ++ show i; \
-             input = namedGlobal nom; }}\
-;\
-instance ToProgram (GlobPull (Exp t)) (Final (GProgram a)) where { \
-  toProgram i f (GlobPull ixf) = \
-    ([(nom,Pointer t)],CG.runPrg (cheat (f input))) \
-      where {nom = "input" ++ show i; \
-             input = namedGlobal nom; }}\
+instance (Scalar t) => ToProgram (GlobPull (Exp t)) (Final (GProgram a)) where
+  toProgram i f (GlobPull ixf) = ([(nom,Pointer t)],CG.runPrg (cheat (f input))) 
+      where nom = "input" ++ show i 
+            input = namedGlobal nom
+            t = typeOf_ (undefined :: t)
 
-                                                              
-toprgBase(Int)
+instance (Scalar t, ToProgram b c) => ToProgram (Exp t) (b -> c) where
+  toProgram i f (a :-> rest) = ((nom,t):ins,prg)
+    where
+      (ins,prg) = toProgram (i+1) (f input) rest
+      nom = "s" ++ show i
+      input = variable nom
+      t = typeOf_ (undefined :: t)
 
-toprgBase(Int8)
-toprgBase(Int16)
-toprgBase(Int32)
-toprgBase(Int64)
+instance (Scalar t, ToProgram b c) => ToProgram (GlobPull (Exp t)) (b -> c) where
+  toProgram i f ((GlobPull ixf) :-> rest) = ((nom,Pointer t):ins,prg)
+    where
+      (ins,prg) = toProgram (i+1) (f input) rest
+      nom = "input" ++ show i
+      input = namedGlobal nom
+      t = typeOf_ (undefined :: t)
 
-toprgBase(Word)
-toprgBase(Word8)
-toprgBase(Word16)
-toprgBase(Word32)
-toprgBase(Word64)
-
-toprgBase(Float)
-toprgBase(Double) 
-
-
-{-
-instance ToProgram b c => ToProgram (Distrib (Pull (Exp t))) (b -> c) where{\
-  toProgram i f ((Distrib blkf) :-> rest) = ((nom,Pointer t):ins,prg)\
-    where {\
-      (ins,prg) = toProgram (i+1) (f input) rest;\
-      nom = "input" ++ show i;\
-      n   = len (blkf (variable "X"));\
-      input = namedGlobal  nom n;}}\
--} 
- 
-#define toprgRec(t) \
-instance ToProgram b c => ToProgram (Exp t) (b -> c) where{\
-  toProgram i f (a :-> rest) = \
-    ((nom,t):ins,prg) \
-    where {\
-      (ins,prg) = toProgram (i+1) (f input) rest;\
-      nom = "s" ++ show i;\
-      input = variable nom;}}\
-;\
-instance ToProgram b c => ToProgram (GlobPull (Exp t)) (b -> c) where{\
-  toProgram i f ((GlobPull ixf) :-> rest) = ((nom,Pointer t):ins,prg)\
-    where {\
-      (ins,prg) = toProgram (i+1) (f input) rest;\
-      nom = "input" ++ show i;\
-      input = namedGlobal nom;}}\
-
-toprgRec(Int)
-
-toprgRec(Int8)
-toprgRec(Int16)
-toprgRec(Int32)
-toprgRec(Int64)
-
-toprgRec(Word)
-toprgRec(Word8)
-toprgRec(Word16)
-toprgRec(Word32)
-toprgRec(Word64)
-
-toprgRec(Float)
-toprgRec(Double) 
-
-      
 ---------------------------------------------------------------------------
 -- heterogeneous lists of inputs 
 ---------------------------------------------------------------------------
@@ -160,37 +99,11 @@ infixr 5 :->
 -- Function types to input list types. 
 --------------------------------------------------------------------------- 
 type family Ips a b
-type family Ips' a 
-
--- type instance Ips' (Exp a) = Exp a
-
--- It seems I need to be this specific for some reason.
--- The commented line above is not enough! 
-#define ipsBase(t) \
-type instance Ips' (Exp t) = Exp t;\
-
-type instance Ips' (GlobPull (Exp t)) = GlobPull (Exp t);
-
-
-ipsBase(Int)
-ipsBase(Int8)
-ipsBase(Int16)
-ipsBase(Int32)
-ipsBase(Int64)
-ipsBase(Word)
-ipsBase(Word8)
-ipsBase(Word16)
-ipsBase(Word32)
-ipsBase(Word64)
-
-ipsBase(Float)
-ipsBase(Double)
-ipsBase(Bool) 
  
 -- type instance Ips a (GlobArray b) = Ips' a -- added Now 26
-type instance Ips a (Final (GProgram b)) = Ips' a 
-type instance Ips a (GProgram b) = Ips' a
-type instance Ips a (b -> c) =  Ips' a :-> Ips b c
+type instance Ips a (Final (GProgram b)) = a 
+type instance Ips a (GProgram b) = a
+type instance Ips a (b -> c) =  a :-> Ips b c
 
 {- TODO:
     What about Distrib (Array p1 a1, Array p2 a2)
@@ -198,3 +111,4 @@ type instance Ips a (b -> c) =  Ips' a :-> Ips b c
 
 
 -} 
+
