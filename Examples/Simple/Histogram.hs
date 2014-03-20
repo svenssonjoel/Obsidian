d9e6fa5573fa2192795b838c131647aa54c64868
Joel Svensson <svenssonjoel@yahoo.se>
2014-03-20 15:03:58 -0400

    histogram

--- Examples/Simple/Histogram.hs
+++ Examples/Simple/Histogram.hs
@@ -1,20 +1,16 @@
 
-module Histogram (histogram) where -- , getHistogram) where
+module Histogram (histogram) where 
 
 import Obsidian
 
 import Prelude
 
--- This example illustrates a problem with Mutable global arrays.
--- They currently have static size. (problem, not a problem... i dont know)
--- Also there is a special mutLen function. Not nice, should be len as for any other array.
 
 histogram :: EWord32    -- blocks
              -> EWord32 -- threads
              -> Mutable Global EWord32
              -> DPull EWord32 -> GProgram ()
 histogram blocks threads mut arr =
-  do
     forAll2 blocks threads $ \bid tid ->
       atomicInc (arr ! (bid * threads + tid)) mut
 
@@ -27,3 +23,4 @@ inputM = undefinedMutable (variable "X")
 
 -- getHistogram = putStrLn $ genKernel "histo" (histogram 256 256) (inputM :- input :- ())
 
+         
