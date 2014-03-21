49d43c4799c2a303880069af6de1f1c0f5328838
Ryan Newton <rrnewton@gmail.com>
2014-03-21 11:51:44 -0400

    Begin to add rudimentary HSbencher scripts

---
+++ Examples/run_benchmarks.hs
@@ -0,0 +1,54 @@
+#!/usr/bin/env runghc
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- | This script runs all Harlan benchmarks.  It is based on a Haskell
+-- benchmarking framework called HSBencher.  Its main advantage is
+-- that it supports uploading of benchmark timings to a Google Fusion
+-- Table.
+
+-- Requires hsbencher >= 0.2
+
+import Control.Monad
+import Data.Maybe
+import qualified Data.ByteString.Char8 as B
+import System.Directory
+import System.FilePath
+import System.Exit
+import System.Environment (getArgs)
+import System.Process
+import GHC.Conc (getNumProcessors)
+import System.IO.Unsafe (unsafePerformIO)
+import Debug.Trace
+
+import HSBencher (defaultMainModifyConfig)
+import HSBencher.Types
+import HSBencher.Methods (makeMethod)
+import HSBencher.Logging (log)
+import HSBencher.MeasureProcess
+import HSBencher.Utils (runLogged, defaultTimeout)
+import Prelude hiding (log)
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = defaultMainModifyConfig myconf
+
+all_benchmarks :: [Benchmark DefaultParamMeaning]
+all_benchmarks =
+  [ 
+     -- mkBenchmark "test/bench-add-vector.kfc" [] defaultCfgSpc
+  ]
+
+-- | Default configuration space over which to vary settings:
+--   This is a combination of And/Or boolean operations, with the ability
+--   to set various environment and compile options.
+defaultCfgSpc = And []
+  where
+--   gpu = And [ Set (Variant "GPU") (RuntimeEnv "HARLAN_DEVICE" "gpu") ]
+
+
+-- | Here we have the option of changing the HSBencher config
+myconf :: Config -> Config
+myconf conf =
+  conf
+   { benchlist = all_benchmarks
+   }
