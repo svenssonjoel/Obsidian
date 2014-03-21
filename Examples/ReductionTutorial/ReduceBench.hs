0d743cc23407209a405b3f4ae504660c809eb61d
Joel Svensson <svenssonjoel@yahoo.se>
2014-03-21 11:15:13 -0400

    update benchmark

--- Examples/ReductionTutorial/ReduceBench.hs
+++ Examples/ReductionTutorial/ReduceBench.hs
@@ -25,87 +25,6 @@ import System.Exit
 
 import Data.IORef
 
-performSmall n threads r = 
-  withCUDA $
-  do
-    kern <- capture threads (r (+) . splitUp n)
-
-    (inputs :: V.Vector Word32) <- lift $ mkRandomVec (fromIntegral n)
-   
-    let cpuresult = V.sum inputs 
-    
-    useVector inputs $ \i ->
-      allocaVector 1 $ \o -> 
-      do
-        o <== (1,kern) <> i 
-        r <- peekCUDAVector o
-        lift $ putStrLn $ show r
-        lift $ putStrLn $ "compare CPU GPU results equal?: " ++ show ((r P.!! 0) == cpuresult)
-
-all512 = [performSmall 512 256 mapRed1,
-          performSmall 512 256 mapRed2,
-          performSmall 512 256 mapRed3,
-          performSmall 512 64 mapRed4,
-          performSmall 512 64 mapRed5,
-          performSmall 512 32 mapRed6,
-          performSmall 512 16 mapRed7]
-
--- all4096 = [performSmall 4096 mapRed1,
---            performSmall 4096 mapRed2,
---            performSmall 4096 mapRed3,
---            performSmall 4096 mapRed4,
---            performSmall 4096 mapRed5,
---            performSmall 4096 mapRed6,
---            performSmall 4096 mapRed7]
-
-           
-
-performAll512 = sequence_ all512
-
---performAll4096 = sequence_ all4096
-
--- ######################################################################
--- Experiment (works only for specific blks/elts combos
-performLarge blcks elts r  =
-  withCUDA $
-  do
-    kern1 <- capture 32 (r (+) . splitUp elts)
-    kern2 <- capture 64 (r (+) . splitUp elts)
-    kern3 <- capture 96 (r (+) . splitUp elts)
-    kern4 <- capture 128 (r (+) . splitUp elts)
-    kern5 <- capture 160 (r (+) . splitUp elts)
-    kern6 <- capture 192 (r (+) . splitUp elts)
-    kern7 <- capture 256 (r (+) . splitUp elts)
-                                           
-
-    (inputs :: V.Vector Word32) <- lift $ mkRandomVec (fromIntegral (blcks * elts))
-    
-    let cpuresult = V.sum inputs 
-    
-    useVector inputs $ \i ->
-      allocaVector (fromIntegral blcks)  $ \(o :: CUDAVector Word32) ->
-        allocaVector 1  $ \(o2 :: CUDAVector Word32) -> do 
-
-        body cpuresult kern1 i o o2
-        body cpuresult kern2 i o o2
-        body cpuresult kern3 i o o2
-        body cpuresult kern4 i o o2
-        body cpuresult kern5 i o o2
-        body cpuresult kern6 i o o2
-        body cpuresult kern7 i o o2
-                                                        
-  where
-    body cpuresult kern i o o2 = 
-        do
-          fill o 0 
-          o <== (blcks,kern) <> i
-          o2 <== (1,kern) <> o 
-
-          r <- peekCUDAVector o2
-          lift $ putStrLn $ show r
-          lift $ putStrLn $ "compare CPU GPU results equal?: " ++ show ((r P.!! 0) == cpuresult)
-
-
 
 -- ######################################################################
 -- Main
@@ -172,4 +91,4 @@ runBenchmark kern t elts =
         
 
           t_tot <- lift $  readIORef t 
-          lift $ putStrLn $ "SELFTIMED: " ++ show t_tot -- (t2 - t1) 
+          lift $ putStrLn $ "SELFTIMED: " ++ show t_tot 
