17d9712a27dab4d703d0b15db02ec9196071edb2
Joel Svensson <svenssonjoel@yahoo.se>
2014-12-02 17:36:53 -0500

    update

--- Examples/Intro/Intro.hs
+++ Examples/Intro/Intro.hs
@@ -29,8 +29,9 @@ incrementAndReverse = reverse . increment
 increment2 :: (Storable a, Forceable t, Num a) => SPull a -> Program t (SPull a)
 increment2 arr = computePull $ fmap (+1) arr
 
--- Programming the Hierarchy
 
+
+-- Programming the Hierarchy
 incrementKernel :: Num a => DPull a -> DPush Grid a
 incrementKernel arr = liftGrid $ fmap (asBlock . increment) arr'
   where
@@ -38,46 +39,29 @@ incrementKernel arr = liftGrid $ fmap (asBlock . increment) arr'
     arr' = splitUp 2048 arr 
 
 incrementKernel2 :: (Data a, Num a) => DPull a -> DPush Grid a
-incrementKernel2  arr = liftGrid $ fmap (execBlock . increment2) arr'
+incrementKernel2  arr =
+  liftGrid $ fmap (execBlock . increment2) arr'
   where
     -- make a selection of how many elements to process per CUDA block
     arr' = splitUp 2048 arr 
 
 incrementKernel3 :: (Data a, Num a) => DPull a -> DPush Grid a
-incrementKernel3 arr = liftGrid $ fmap (liftBlock . (fmap (execWarp . increment2))) arr'
+incrementKernel3 arr =
+  liftGrid $ fmap (liftBlock . (fmap (execWarp . increment2))) arr'
   where
     -- make a selection of how many elements to process per CUDA block/warp
-    arr' = fmap (splitUp 32) $ splitUp 2048 arr 
+    arr' = fmap (splitUp 512) $ splitUp 2048 arr 
 
 
 
-
----------------------------------------------------------------------------
--- Simple Increment example
----------------------------------------------------------------------------
-
--- incLocal :: SPull EWord32 -> SPull EWord32
--- incLocal arr = fmap (+1) arr
-
-
--- increment :: DPull (SPull EWord32) -> DPush Grid EWord32 
--- increment arr = asGridMap body arr
---   where body a = push (incLocal a) :: SPush Block EWord32 
-
-
--- increment' :: DPull (SPull (SPull EWord32)) -> DPush Grid EWord32 
--- increment' arr = asGrid (fmap body arr) 
---   where body a = asBlock (fmap body' a) 
---         body' a  = push $ incLocal a :: SPush Block EWord32
-
+--------------------------------------------------
+-- run the kernel 
 performInc :: IO () 
 performInc =
   withCUDA $
   do
-    -- kern <- capture 256 (increment . splitUp 256)
     kern <- capture 512 incrementKernel3
     
-
     useVector (V.fromList [0..4096 :: Word32]) $ \i ->
       allocaVector 4096  $ \o ->
       do
